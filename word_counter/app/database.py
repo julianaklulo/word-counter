0ca78cc1102f31ac375a727bf6693dda2807f58a
Juliana Karoline <julianaklulo@gmail.com>
2021-08-31 20:17:41 -0300

    Fix linting

--- word_counter/app/database.py
+++ word_counter/app/database.py
@@ -4,7 +4,6 @@ from sqlalchemy.orm import sessionmaker
 
 from app.config import settings
 
-
 engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
