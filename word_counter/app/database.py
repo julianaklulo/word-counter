b139299a00b71b934ef6080f19fc2887fde01bc2
Juliana Karoline <julianaklulo@gmail.com>
2021-09-04 22:16:54 -0300

    Add postgres dependency

--- word_counter/app/database.py
+++ word_counter/app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 from app.config import settings
 
-engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
