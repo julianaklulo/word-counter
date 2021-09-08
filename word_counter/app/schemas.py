d2d45845946f6891dc6017110aa82c88f3142931
Juliana Karoline <julianaklulo@gmail.com>
2021-09-07 22:46:09 -0300

    Remove comment

--- word_counter/app/schemas.py
+++ word_counter/app/schemas.py
@@ -13,6 +13,4 @@ class SubmissionRow(SubmissionCreate):
     id: int
 
     class Config:
-        # will tell pydantic model to read data even if it is not a dict, but a orm model
-        # so id = data["id"] and id = data.id both work
         orm_mode = True
