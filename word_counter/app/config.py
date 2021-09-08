9f4a112bf4034e85af2dc2838cc2b36b28b5f0ac
Juliana Karoline <julianaklulo@gmail.com>
2021-09-07 22:45:48 -0300

    Change config to use pydantic

--- word_counter/app/config.py
+++ word_counter/app/config.py
@@ -1,11 +1,12 @@
-from prettyconf import config
+from pydantic import BaseSettings, Field
 
 
-class Settings:
-    DATABASE_URL = config("DATABASE_URL", default="sqlite:///./word-count.db")
-    TEST_DATABASE_URL = config(
-        "TEST_DATABASE_URL", default="sqlite:///./word-count-test.db"
-    )
+class Settings(BaseSettings):
+    DATABASE_URL: str = Field("sqlite:///./word-count.db")
+    TEST_DATABASE_URL: str = Field("sqlite:///./word-count-test.db")
+
+    class Config:
+        env_file = ".env"
 
 
 settings = Settings()
